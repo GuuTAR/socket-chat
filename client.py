2482996cc8ef675cc4718168f9f132bdc502f009
Tinnapop Pratheep <tinnapop.pratheep@gmail.com>
2021-02-28 17:54:38 +0700

    Add input IP server

--- client.py
+++ client.py
@@ -1,15 +1,11 @@
 import socket
 import threading
 
-SERVER = 'localhost'  # Standard loopback interface address (localhost)
 PORT = 21948       # Port to listen on (non-privileged ports are > 1023)
 
 HEADER_LENGTH = 100
 
 FORMAT = "utf-8"
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER, PORT))
 
 def send_msg() :
     while True :
@@ -21,6 +17,11 @@ def recv_msg() :
         message = client.recv(HEADER_LENGTH)
         if message :
             print(message.decode(FORMAT))
+
+server_ip = input("Please insert server IP : ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server_ip, PORT))
 
 send_msg_thread = threading.Thread(target=send_msg)
 send_msg_thread.start()
